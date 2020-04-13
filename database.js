fc121dd67b066506e9bccd172970a5d4c12ed1ec
Germaine Pannell <me@germainep.com>
2020-04-12 20:01:59 -0400

    completed all crud operations

--- database.js
+++ database.js
@@ -1,17 +1,47 @@
-const users = [];
+let users = [
+    { id: '1', name: 'Germaine', bio: 'testing' },
+    { id: '2', name: 'james', bio: 'testing 2' },
+    { id: '3', name: 'jumper', bio: 'third' }
+]
 
-getUsers = () => {
-    return users;
-};
+const getUsers = () => {
+    return users
+}
 
-getUserById = (id) => {
-    users.find((user) => {
-        return user.id === id;
-    });
-};
+const getUserById = ( id ) => {
+    return users.find(( user ) => {
+        return user.id === id
+    })
+}
 
+const createUser = ( data ) => {
+    const payload = data
+    users.push(payload)
+    console.log(data)
+    return payload
+}
+
+const updateUser = ( id, data ) => {
+    const index = users.findIndex(( user ) => {
+        return user.id === id
+    })
+    users[index] = {
+        ...users[index],
+        ...data
+    }
+    return users[index]
+}
+
+const deleteUser = ( id ) => {
+    users = users.filter(( user ) => {
+        return id !== user.id
+    })
+    console.log(users)
+}
 module.exports = {
     getUsers: getUsers,
-    getUserById: getUserById
-
-};
+    getUserById: getUserById,
+    createUser: createUser,
+    deleteUser: deleteUser,
+    updateUser
+}
--- server.js
+++ server.js
@@ -1,28 +1,65 @@
-const express = require('express');
-const db = require('./database.js');
-const server = express();
+const express = require('express')
+const shortid = require('shortid')
+const cors = require('cors')
 
-server.get('/api/users', (req, res) => {
-    console.log(db);
-    const users = db.getUsers();
-    res.json(users);
-});
+const db = require('./database.js')
+const server = express()
 
-server.get('/api/users/:id', (req, res) => {
-    const userId = req.param.id;
-    const user = db.getUserById(userId);
+server.use(express.json())
+server.use(cors())
 
-    if (user) {
-        res.json(user);
+server.get('/api/users', ( req, res ) => {
+    const users = db.getUsers()
+    res.json(users)
+})
+
+server.get('/api/users/:id', ( req, res ) => {
+    const userId = req.params.id
+    const user = db.getUserById(userId)
+    if (!user) {
+        res.status(404).json({ message: 'User not found' })
     }
-    res.status(404).json({ message: 'user not found' });
+    res.json(user)
 });
 
 server.post('/api/users', (req, res) => {
-    const newUserName = req.body.name;
-    const newUserBio = req.body.bio;
+    if (!req.body.name || !req.body.bio) {
+        return res.status(404).json({ message: 'Missing information' })
+    }
+    const newUser = db.createUser({
+                                      id: shortid.generate(),
+                                      name: req.body.name,
+                                      bio: req.body.bio,
+                                  })
+    res.status(201).json(newUser)
 });
 
+server.delete('/api/users/:id', ( req, res ) => {
+    const user = db.getUserById(req.params.id)
+
+    if (!user) {
+        res.status(404).json({ message: 'user not found' })
+    }
+    db.deleteUser(user.id)
+    res.status(204).end()
+})
+
+server.put('/api/users/:id', ( req, res ) => {
+    const user = db.getUserById(req.params.id)
+
+    if (user) {
+        const updatedUser = db.updateUser(user.id, {
+            name: req.body.name || user.name,
+            bio: req.body.bio || user.bio
+        })
+        res.json(updatedUser)
+    }
+    else {
+        res.status(404).json({ message: 'User not found' })
+    }
+})
+
+
 server.listen(8080, () => {
-    console.log('listening on port 8080');
-});
+    console.log('listening on port 8080')
+})
